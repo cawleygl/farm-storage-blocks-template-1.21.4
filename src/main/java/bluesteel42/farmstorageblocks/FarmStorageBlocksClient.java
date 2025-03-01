4f2ed149d41461f7a7d8b90cbf57791bd402e453
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-02-28 21:45:54 -0500

    Initial commit

---
+++ src/main/java/bluesteel42/farmstorageblocks/FarmStorageBlocksClient.java
@@ -0,0 +1,10 @@
+package bluesteel42.farmstorageblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FarmStorageBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
